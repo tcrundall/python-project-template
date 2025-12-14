23ebb0a9d420568d3a75b64792e7a57be9fc0d30
Timothy Crundall <tim.crundall@gmail.com>
2025-12-14 14:10:49 +0100

    Add example simple excel data pipeline

--- src/main.py
+++ src/main.py
@@ -1,11 +1,54 @@
-import example
+import sys
+import json
+import pandas as pd
 
+COLUMN_MAPPINGS_KEY = "column-mappings"
 
-def my_func():
-    print("I am a function")
-
+USAGE = """
+        Usage: python main.py my-config-file.json my-data.xlsx
+        """
 
 if __name__ == "__main__":
-    print("I am being executed as a script")
-    my_func()
-    print("example.add result: ", example.add(1, 2))
+    # ----------------------------------------
+    #      Parse and validate user input
+    # ----------------------------------------
+    if len(sys.argv) != 3:
+        print(USAGE)
+        sys.exit(1)
+
+    config_file = sys.argv[1]
+    excel_file = sys.argv[2]
+
+    if config_file.split(".")[-1] != "json":
+        print("Expected a json file as first argument")
+        print(USAGE)
+        sys.exit(1)
+
+    if excel_file.split(".")[-1] != "xlsx":
+        print("Expected a json file as first argument")
+        print(USAGE)
+        sys.exit(1)
+
+    # ----------------------------------------
+    #      Read config and data
+    # ----------------------------------------
+    with open(config_file) as fp:
+        config = json.load(fp)
+    print(config)
+    for key, val in config[COLUMN_MAPPINGS_KEY].items():
+        print(f"{key=}, {val=}")
+
+    df = pd.read_excel(excel_file)
+    print(df)
+
+    # ----------------------------------------
+    #      Clean data
+    # ----------------------------------------
+    df = df.rename(columns=config[COLUMN_MAPPINGS_KEY])
+    print(f"Cleaned data: {df}")
+
+    # ----------------------------------------
+    #      Analyze data
+    # ----------------------------------------
+    sum = df.sum(numeric_only=True)
+    print(sum)
